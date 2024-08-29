5ba4fba03e1cb9b7e0232bd9757bcdc2b1551f66
simurgh101 <suthiwatpraesi@hotmail.com>
2024-08-29 21:29:57 +1000

    pre 90 precent complete

--- Assets/Scripts/PrintAndHide.cs
+++ Assets/Scripts/PrintAndHide.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class ConsolePrint : MonoBehaviour
 {
     private int i = 3;
+    public Renderer rend;
     //public GameObject gameObject;
     // Start is called before the first frame update
     void Start()
